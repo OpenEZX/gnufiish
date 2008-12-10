7c35c7e5ba82f2c3a84be384545e2345783e7113
Harald Welte <laforge@gnumonks.org>
2008-12-10 10:29:20 +0100

    M800: don't add I2C device for Codec, ASoC handles this

--- arch/arm/mach-s3c2440/mach-glofiish.c
+++ arch/arm/mach-s3c2440/mach-glofiish.c
@@ -277,13 +277,10 @@ static struct i2c_board_info glofiish_i2c_devs[] __initdata = {
 		.irq = M800_IRQ_FMRADIO,
 	},
 	{
-		/* Audio Codec */
-		I2C_BOARD_INFO("ak4641", 0x12),
-	},
-	{
 		/* Battery (unknown chip) */
 		I2C_BOARD_INFO("gf_batt", 0x2f),
 	},
+	/* Don't put Audio Codec here, ASoC handles it */
 };
 
 static struct s3c2410_nand_set glofiish_nand_sets[] = {
